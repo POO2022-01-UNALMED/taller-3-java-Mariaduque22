a497a8a965e230894f4ca7ca3abfec50f0899d4f
Maria Paula Duque <maduquem@unal.edu.co>
2022-03-28 22:00:06 -0500

    cry

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,48 +1,48 @@
 package taller3.televisores;
 
-public class Control{
-	private TV tv;
+public class Control {
 
-	public void turnOff(){
-		tv.turnOff();
-	}
-	
-	
-    	public void setCanal(int canal){
-        	this.tv.setCanal(canal);
-    	}
-	
-	public void turnOn(){
-		this.tv.turnOn();
-	}
-	
-	public void canalUp(){
-		this.tv.canalUp();
-	}
+    private TV tv;
+    
+    public void enlazar(TV tv){
+        this.tv = tv;
+        this.tv.setControl(this);
+    }
 
-	public void canalDown(){
-		this.tv.canalDown();
-	}
-	
-	public void volumenUp(){
-		this.tv.volumenUp();
-	}
+    public void setTv(TV tv){
+        this.tv = tv;
+    }
 
-	public void volumenDown(){
-		this.tv.volumenDown();
-	}
-	
-  	public void enlazar(TV tv){
-        	this.tv = tv;
-        	this.tv.setControl(this);
-    	}
-	
+    public TV getTv() {
+        return tv;
+    }
 
-	public TV getTv(){
-		return tv;
-	}
+    public void turnOn(){
+        this.tv.turnOn();
+    }
 
-	public void setTv(TV tv){
-		this.tv=tv;
-	}
+    public void turnOff(){
+        this.tv.turnOff();
+    }
+
+    public void canalUp(){
+        this.tv.canalUp();
+    }
+
+    public void canalDown(){
+        this.tv.canalDown();
+    }
+
+    public void volumenUp(){
+        this.tv.volumenUp();
+    }
+
+    public void volumenDown(){
+        this.tv.volumenDown();
+    }
+
+    public void setCanal(int canal){
+        this.tv.setCanal(canal);
+    }
+
 }
